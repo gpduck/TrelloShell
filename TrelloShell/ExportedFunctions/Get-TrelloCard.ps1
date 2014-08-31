8168e619887cb51c45c722212fedf2e8f21b3715
Chris Duck <gpduck@whatsupduck.net>
2014-08-30 22:21:44 -0500

    Get cards

---
+++ TrelloShell/ExportedFunctions/Get-TrelloCard.ps1
@@ -0,0 +1,38 @@
+function Get-TrelloCard {
+  param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ParameterSetName='ForBoard')]
+    [TrelloNet.IBoardId]$board,
+
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ParameterSetName='ForList')]
+    [TrelloNet.IListId]$list,
+
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ParameterSetName='ForMember')]
+    [TrelloNet.IMemberId]$member,
+
+    
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ParameterSetName='ForChecklist')]
+    [TrelloNet.IChecklistId]$checklist,
+
+    [Parameter(Mandatory=$False,ValueFromPipeline=$False,ParameterSetName='ForList')]
+    [Parameter(Mandatory=$False,ValueFromPipeline=$False,ParameterSetName='ForMember')]
+    [Parameter(Mandatory=$False,ValueFromPipeline=$True,ParameterSetName='ForMe')]
+    [Parameter(Mandatory=$False,ValueFromPipeline=$False,ParameterSetName='ForChecklist')]
+    [TrelloNet.CardFilter]$filter,
+
+    [String]$Name
+  )
+  begin {
+    $Trello = Get-Trello
+  }
+  process {
+    $WhereScript = {
+      if($Name) {
+        $_.Name -like $Name
+      } else {
+        $True
+      }
+    }
+    Invoke-TrelloObjectMethod -Object $Trello.Cards -Name $PSCmdlet.ParameterSetName -Parameters $PSBoundParameters | where -FilterScript $WhereScript
+  }
+}
+Export-ModuleMember -Function Get-TrelloCard
